030e716d4cd87e7b3e28f7230cf3feb932469625
Supreeth <saisupreeth7010@gmail.com>
2025-11-05 20:20:34 +0530

    updated sitename

--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,8 @@
 // API Configuration
 const CONFIG = {
     API_URL: window.location.hostname === 'localhost' 
-        ? 'http://localhost:3000/api' 
-        : 'https://sevendayschallenge.onrender.com/api', // Make sure this matches
+        ? 'http://localhost:3000/api'
+        : 'https://projectsevendays.netlify.app/api', // Make sure this matches
     USER_ID: 'user_' + (localStorage.getItem('7jacked_user_id') || generateUserId())
 };
 
